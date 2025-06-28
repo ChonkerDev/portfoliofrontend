7cd08ad1873b1b7fb5ac0f1da470c5bb3bd11212
ChonkerDev <163789588+ChonkerDev@users.noreply.github.com>
2025-06-28 17:47:17 -0500

    added video to project lilith

--- src/pages/Projects/ProjectProjectLilith.tsx
+++ src/pages/Projects/ProjectProjectLilith.tsx
@@ -1,13 +1,38 @@
 import SlideFade from '../../components/animation/SlideFade';
-import CenteredDiv from '../../components/layout/CenteredDiv';
+import {Typography} from "@mui/material";
 
+const videoUrl = "https://f005.backblazeb2.com/file/ChonkerPortfolio/Showcase+Project+Lilith.mp4";
 
 export default function ProjectProjectLilith() {
-  return <SlideFade>
-    <CenteredDiv>
-    <h2>Header</h2>
-    <p>Details</p>
-    </CenteredDiv>
-  </SlideFade>
+    return <SlideFade>
+        <div>
+            <div style={{justifyItems: 'center'}}>
+                <h2>Header</h2>
+                <p>Details</p>
+            </div>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'start',
+                alignItems: 'start',
+                height: '100vh',
+                flexDirection: 'column'
+            }}>
+                <Typography variant="h4" component="h2" gutterBottom>
+                    Showcase Project Lilith
+                </Typography>
+            <video
+                width="40%"
+                controls
+                preload="metadata"
+                poster="https://f000.backblazeb2.com/file/your-bucket/thumb.jpg"
+                autoPlay={false}
+                style={{border: '4px solid red'}}
+            >
+                <source src={videoUrl} type="video/mp4"/>
+                Your browser does not support the video tag.
+            </video>
+        </div>
+    </div>
+</SlideFade>
 
 }
