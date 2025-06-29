474535ef6222d944c2a5e8493cbdb1951ad84ffd
ChonkerDev <163789588+ChonkerDev@users.noreply.github.com>
2025-06-28 22:57:22 -0500

    refactored project lilith rendering

--- src/pages/Projects/ProjectProjectLilith.tsx
+++ src/pages/Projects/ProjectProjectLilith.tsx
@@ -1,19 +1,12 @@
 import SlideFade from '../../components/animation/SlideFade';
 import {Typography} from "@mui/material";
 
-const videoUrl = "https://www.youtube.com/embed/wyZAEKPqAH8";
+
 
 export default function ProjectProjectLilith() {
-    return <SlideFade>
+    return (<SlideFade>
         <div>
-            <div style={{display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',      // <-- this ensures horizontal centering
-                justifyContent: 'center',  // optional if you want vertical centering too
-                textAlign: 'center',}}>
-                <h2>Header</h2>
-                <p>Details</p>
-            </div>
+            {renderHeader()}
             <div style={{
                 display: 'flex',
                 justifyContent: 'start',
@@ -24,24 +17,43 @@ export default function ProjectProjectLilith() {
                 <Typography variant="h4" component="h2" gutterBottom>
                     Showcase Project Lilith
                 </Typography>
-                <div style={{ position: 'relative', width: '40%', paddingTop: '22.5%' /* 16:9 aspect ratio */ }}>
-                    <iframe
-                        src={videoUrl}
-                        title="YouTube video player"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                        style={{
-                            position: 'absolute',
-                            top: 0,
-                            left: 0,
-                            width: '100%',
-                            height: '100%',
-                            border: 0
-                        }}
-                    ></iframe>
-                </div>
+                {renderVideo()}
+            </div>
         </div>
-    </div>
-</SlideFade>
+    </SlideFade>);
+}
 
+const renderHeader = () => {
+    return (
+        <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'center',
+            textAlign: 'center',
+        }}>
+            <h2>Header</h2>
+            <p>Details</p>
+        </div>
+    );
+}
+
+const renderVideo = () => {
+    const videoUrl = "https://www.youtube.com/embed/wyZAEKPqAH8";
+    return (<div style={{position: 'relative', width: '40%', paddingTop: '22.5%' /* 16:9 aspect ratio */}}>
+        <iframe
+            src={videoUrl}
+            title="YouTube video player"
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+                border: 0
+            }}
+        ></iframe>
+    </div>);
 }
--- src/pages/Projects/ProjectSynthRace.tsx
+++ src/pages/Projects/ProjectSynthRace.tsx
@@ -1,17 +1,46 @@
 import SlideFade from '../../components/animation/SlideFade';
-import CenteredDiv from '../../components/layout/CenteredDiv';
+import {Typography} from "@mui/material";
 
 const steamlink = "https://store.steampowered.com/widget/2944360/";
+const videoUrl = "https://www.youtube.com/embed/F8sk8UB1kxM";
 
 export default function ProjectSynthRace() {
-    return <CenteredDiv>
-    <SlideFade>
-        <iframe
-            src={steamlink}
-            frameBorder="0"
-            width="646"
-            height="190">
-        </iframe>
+    return <SlideFade>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+            <iframe
+                src={steamlink}
+                frameBorder="0"
+                width="646"
+                height="190"
+            ></iframe>
+        </div>
+        <div style={{
+            display: 'flex',
+            justifyContent: 'start',
+            alignItems: 'start',
+            height: '100vh',
+            flexDirection: 'column'
+        }}>
+            <Typography variant="h4" component="h2" gutterBottom>
+                Synth Race Trailer
+            </Typography>
+            <div style={{position: 'relative', width: '40%', paddingTop: '22.5%' /* 16:9 aspect ratio */}}>
+                <iframe
+                    src={videoUrl}
+                    title="YouTube video player"
+                    allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    style={{
+                        position: 'absolute',
+                        top: 0,
+                        left: 0,
+                        width: '100%',
+                        height: '100%',
+                        border: 0
+                    }}
+                ></iframe>
+            </div>
+        </div>
     </SlideFade>
-    </CenteredDiv>
+
 }
